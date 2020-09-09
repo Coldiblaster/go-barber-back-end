5f57b0e4c1bf4e717f83a2a71846bd96ef61917a
Coldiblaster <vbastazin@gmail.com>
2020-09-08 22:14:12 -0300

    Finalizando tratamento de exceções

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,6 +1,8 @@
 import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 
+import AppError from '../errors/AppError';
+
 import User from '../models/User';
 
 interface Request {
@@ -18,7 +20,7 @@ class CreateUserService {
     });
 
     if (checkUserExists) {
-      throw new Error('Email address alteady used.');
+      throw new AppError('Email address alteady used.');
     }
 
     const hashedPassword = await hash(password, 8);
