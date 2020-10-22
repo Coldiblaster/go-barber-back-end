3f44c0f812577f9b453b76df4baacd41d1390b9b
Coldiblaster <vbastazin@gmail.com>
2020-10-22 13:58:28 -0300

    Add Jest

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,9 @@
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppoitment', () => {
+  it('should be able to create a new appointment', () => {});
+
+  // it('should not be able to create two appointments on the same time', () => {
+  //   expect(1 + 2).toBe(3);
+  // });
+});
